87c2218d268d8f1b0ada3ec8ebe1373dc2aaa681
wangyu <wangyu@gcks.cn>
2017-08-17 15:12:56 +0800

    Use Spring AOP to reconstruct the sprice.

--- sprice/src/main/java/practice/spring/sprice/Assistant.java
+++ sprice/src/main/java/practice/spring/sprice/Assistant.java
@@ -1,7 +1,15 @@
 package practice.spring.sprice;
 
+import org.aspectj.lang.JoinPoint;
+
 public class Assistant {
     public void logProductProgress(String productName, String progress) {
         System.out.println(String.format("product=%s, progress=%s", productName, progress));
+    }
+
+    public void afterQueryProgress(JoinPoint joinPoint, String progress) {
+        System.out.println("Assistant.afterQueryProgress");
+        String productName = (String)joinPoint.getArgs()[0];
+        logProductProgress(productName, progress);
     }
 }
--- sprice/src/main/java/practice/spring/sprice/ProductManager.java
+++ sprice/src/main/java/practice/spring/sprice/ProductManager.java
@@ -2,20 +2,14 @@ package practice.spring.sprice;
 
 public class ProductManager implements Manager {
     private Engineer engineer;
-    private Assistant assistant;
 
     @Override
     public String queryProgress(String productName) {
         String progress = this.engineer.reportProgress(productName);
-        this.assistant.logProductProgress(productName, progress);
         return progress;
     }
 
     public void setEngineer(Engineer engineer) {
         this.engineer = engineer;
-    }
-
-    public void setAssistant(Assistant assistant) {
-        this.assistant = assistant;
     }
 }
